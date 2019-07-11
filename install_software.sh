f17d7340c41512ea9fa5e7d48f3e52cb96bc7b5b
Salvatore Rappoccio <rappoccio@gmail.com>
2019-07-11 14:33:35 -0400

    Add swig

--- install_software.sh
+++ install_software.sh
@@ -1,5 +1,5 @@
 # Ubuntu+tools, python, pip, python modules
-apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y wget g++ libtool rsync make x11-apps tzdata python3-dev python3-numpy python3-pip python3-tk
+apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y wget g++ libtool rsync make x11-apps tzdata python3-dev python3-numpy python3-pip python3-tk swig
 rm -rf /var/lib/apt/lists/*
 pip3 install --upgrade pip && pip install --no-cache-dir matplotlib scipy numpy scikit-learn keras tensorflow ipykernel ipython jupyter metakernel zmq notebook
 
