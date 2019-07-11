9da8210f87cc9b37288de6ea95e7f3ec38756784
Salvatore Rappoccio <rappoccio@gmail.com>
2019-07-11 14:16:55 -0400

    Converting mutabledemo to py3

--- ReviewPython/mutabledemo.py
+++ ReviewPython/mutabledemo.py
@@ -12,34 +12,34 @@ def mod_str(x):
 def main():
     x = 0
 
-    print 'before increment, x = ', x
+    print( 'before increment, x = ', x )
     increment(x)
-    print 'after  increment, x = ', x
+    print( 'after  increment, x = ', x )
 
     xvals = [0,1,2,3,9]
-    print 'before append_one, xvals ='
-    print xvals
+    print( 'before append_one, xvals =' )
+    print( xvals )
     append_one(xvals)
-    print 'after  append_one, xvals ='
-    print xvals
+    print( 'after  append_one, xvals =' )
+    print( xvals )
 
     s = 'noooooo'
-    print 'before mod_str, s ='
-    print s
+    print( 'before mod_str, s =' )
+    print( s )
     mod_str(s)
-    print 'after  mod_str, s ='
-    print s
+    print( 'after  mod_str, s =' )
+    print( s )
 
     # BAD: many copies since string is immutable:
     v = "Always"
     for vi in [ "Look", "On", "The", "Bright", "Side", "Of", "Life"]:
         v += vi
 
-    print 'v is ', v
+    print( 'v is ', v )
 
     # Good: make a mutable list and join:
     w = "".join( [ "Always", "Look", "On", "The", "Bright", "Side", "Of", "Life"] )
-    print 'w is ', w
+    print( 'w is ', w )
 
 
 if __name__ == "__main__":
