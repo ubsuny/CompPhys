87fd29126fc80d1cbc307490abffaba47641a5e2
Salvatore Rappoccio <rappoccio@gmail.com>
2019-08-28 14:59:59 -0400

    Not looking for linux in string of runDocker

--- runDocker.sh
+++ runDocker.sh
@@ -12,7 +12,7 @@ if [ "$(uname)" == "Darwin" ]; then
     ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
     xhost + $ip
     docker run --rm -it -e DISPLAY=$ip:0  -v /tmp/.X11-unix:/tmp/.X11-unix -v ${PWD}/../results:/results ${icommand} -p 8888:8888 $1
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+else; then
 
     if [ $# -eq 2 -a $2 -eq 1 ]
     then
