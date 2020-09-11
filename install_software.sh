3e1f236fa1220495dd515b658140ff97d9872db6
Thomay <thomay@timthoma-mbp.physics.buffalo.edu>
2020-09-11 14:52:11 -0400

    added pytest and Jupyter Notebook unit test nbval

--- install_software.sh
+++ install_software.sh
@@ -1,6 +1,5 @@
 # Ubuntu+tools, python, pip, python modules
 apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y wget g++ libtool rsync make x11-apps tzdata python3-dev python3-numpy python3-pip python3-tk swig
 rm -rf /var/lib/apt/lists/*
-pip3 install --upgrade pip && pip install --no-cache-dir matplotlib scipy numpy scikit-learn keras tensorflow ipykernel ipython jupyter metakernel zmq notebook
-
-
+# tensorflow requires scipy==1.4.1 not newer!
+pip3 install --upgrade pip && pip install --no-cache-dir matplotlib scipy==1.4.1 numpy scikit-learn keras tensorflow ipykernel ipython jupyter metakernel zmq notebook pytest nbval
