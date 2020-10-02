b68d430aacb0dfe6d6d4d1f2470e8b0013ee00a6
Tim <thomay@buffalo.edu>
2020-10-02 11:49:01 -0400

    added examples for linting

---
+++ ReviewPython/docstrings/oneline.py
@@ -0,0 +1,5 @@
+def oneline():
+    """This is a one line docstring"""
+    global _olvar
+    if _olvar:
+        return _olvar
---
+++ ReviewPython/linting/better_hello.py
@@ -0,0 +1,7 @@
+
+"""
+This is the hello world module
+"""
+def hello():
+    """ My Hello World program """
+    print("Hello World")
