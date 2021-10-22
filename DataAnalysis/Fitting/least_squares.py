ab564678e41a19d2a925231e05d22159be674b62
Tim <thomay@buffalo.edu>
2021-10-22 17:28:29 -0400

    added pylintrc and started linting

--- DataAnalysis/Fitting/least_squares.py
+++ DataAnalysis/Fitting/least_squares.py
@@ -1,7 +1,9 @@
+""" define a function to linear fit x-y data without error bars"""
+
+import sys
 import numpy as np
 
-# define a function to linear fit x-y data without error bars
-def least_squares(x, y):    
+def least_squares(x, y):
     """Perform a least-squares fit to data (x,y)
 
     Args :
@@ -17,34 +19,34 @@ def least_squares(x, y):
 
     """
 
-    n = len(x)   # number of galaxies
+    N = len(x)   # number of galaxies
 
-    if n <= 2 :
+    if N <= 2:
         print ('Error! Need at least two data points!')
-        exit()
+        sys.exit()
 
     # Compute all of the stat. variables we need
     s_x = np.sum(x)
     s_y = np.sum(y)
     s_xx = np.sum( x**2 )
-    s_xy = np.sum( x*y )
-    denom = n * s_xx - s_x**2
-    if abs( denom ) < 0.000001 : 
+    s_xy = np.sum( x * y )
+    denom = N * s_xx - s_x**2
+    if abs( denom ) < 0.000001:
         print ('Error! Denomominator is zero!')
-        exit()
+        sys.exit()
 
-    # Compute y-intercept and slope 
+    # Compute y-intercept and slope
     a = (s_xx * s_y - s_x * s_xy) / denom
-    b = (n*s_xy - s_x * s_y) / denom   
+    b = (N * s_xy - s_x * s_y) / denom
 
     # Compute uncertainties
-    if n > 2 : 
-          sigma = np.sqrt(np.sum((y - (a+b*x))**2 ) / (n-2))
-          sigma_a = np.sqrt(sigma**2 * s_xx / denom)
-          sigma_b = np.sqrt(sigma**2 * n / denom)
-    else :
-          sigma = 0.
-          sigma_a = 0.
-          sigma_b = 0.
+    if N > 2:
+        sigma = np.sqrt(np.sum((y - (a + b * x))**2 ) / (N - 2))
+        sigma_a = np.sqrt(sigma**2 * s_xx / denom)
+        sigma_b = np.sqrt(sigma**2 * N / denom)
+    else:
+        sigma = 0.
+        sigma_a = 0.
+        sigma_b = 0.
 
-    return [a, b, sigma, sigma_a, sigma_b]    
+    return [a, b, sigma, sigma_a, sigma_b]
