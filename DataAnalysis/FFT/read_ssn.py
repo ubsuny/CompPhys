2c5e7908284f6271ddff73ee97f0336ed5ff6fb3
Salvatore Rappoccio <rappoccio@gmail.com>
2022-10-17 11:57:52 -0400

    Updating to latest 2022 data in FFT module

--- DataAnalysis/FFT/read_ssn.py
+++ DataAnalysis/FFT/read_ssn.py
@@ -1,13 +1,13 @@
 from numpy import genfromtxt
 
 
-def read_ssn(data_file_name='monthssn.dat', verbose=False):
+def read_ssn(data_file_name='SN_m_tot_V2.0.txt', verbose=False):
     
     # data downloaded from http://www.sidc.be/DATA/yearssn.dat
     print ( ' Sunspot data from Solar Influences Data Analysis Center')
 
 
-    data = genfromtxt(data_file_name,comments='#',usecols=(1,2))
+    data = genfromtxt(data_file_name,comments='#',usecols=(3,4))
 
     dates = data[:,0]
     ssns = data[:,1]
