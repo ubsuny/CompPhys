0ed47a791ca7f623fc347e00ed417a0e2d7eb687
DryRun <david.renhwa.yu@gmail.com>
2025-09-12 11:26:15 -0400

    More array pointer examples

--- ReviewCpp/BasicExamples/array_pointer.cpp
+++ ReviewCpp/BasicExamples/array_pointer.cpp
@@ -2,10 +2,14 @@
  
 int main()
 {
-  int arr[5] = {0, 1, 2, 3, 4};
+  double arr[5] = {0., 1., 2., 3., 4.};
   std::cout << "arr = " << arr << std::endl;
   std::cout << "*arr = " << *arr << std::endl;
+
   std::cout << "arr+1 = " << arr+1 << std::endl;
   std::cout << "*(arr+1) = " << *(arr+1) << std::endl;
+
+  std::cout << "arr+2 = " << arr+2 << std::endl;
+  std::cout << "*(arr+2) = " << *(arr+2) << std::endl;
   return 0;
 }
--- ReviewCpp/BasicExamples/vector_example.cc
+++ ReviewCpp/BasicExamples/vector_example.cc
@@ -3,6 +3,14 @@
  
 int main()
 {
+  int arr[5] = {0, 1, 2, 3, 4};
+  std::cout << "arr = " << arr << std::endl;
+  std::cout << "*arr = " << *arr << std::endl;
+  std::cout << "arr+1 = " << arr+1 << std::endl;
+  std::cout << "*(arr+1) = " << *(arr+1) << std::endl;
+
+
+
   // Create a vector containing integers
   std::vector<int> v = {7, 5, 16, 8};
  
