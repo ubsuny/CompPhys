15891c0fffb5a8ed6db9286e604e78725c7db4b2
DryRun <david.renhwa.yu@gmail.com>
2026-02-13 13:55:14 -0500

    Update QM notebooks

--- QM/vmc.cpp
+++ QM/vmc.cpp
@@ -1,67 +1,70 @@
 // Variational Monte Carlo for the harmonic oscillator
 #include "vmc.h"
 
-QHO::QHO(int Nin, double alphain, int MCStepsin) :
-  rd(), gen(rd()), dis(0,1.), gausdev(),
-  N(Nin), alpha(alphain), MCSteps(MCStepsin)
+QHO::QHO(int N_in, double alpha_in, int MC_steps_in) :
+  rd_(), rng_(rd_()), uniform_(0,1.), gaussian_(),
+  N_(N_in), alpha_(alpha_in), MC_steps_(MC_steps_in)
 {
-  x.resize(N);
-  for (int i = 0; i < N; i++)
-    x[i] = dis(gen)-0.5;
-  delta = 1;
+  x_.resize(N_); // create N_ walkers
+  for (int i = 0; i < N_; i++) {
+    x_[i] = uniform_(rng_)-0.5; // random initial positions in [-0.5, 0.5]
+  }
+  delta_ = 1;
 
-  nPsiSqd = int((xMax - xMin) / dx);
-  psiSqd.resize(nPsiSqd);
-  nAccept = 0;
+  n_psi_squared_ = int((x_max_ - x_min_) / dx_);
+  psi_squared_.resize(n_psi_squared_);
+  n_accept_ = 0;
 
   zeroAccumulators();
 }
 
 void QHO::zeroAccumulators() {
-  eSum = eSqdSum = 0;
-  for (int i = 0; i < nPsiSqd; i++)
-    psiSqd[i] = 0;
+  E_sum_ = 0;
+  E_squared_sum_ = 0;
+  for (int i = 0; i < n_psi_squared_; i++) {
+    psi_squared_[i] = 0;
+  }
 }
 
-double QHO::p(double xTrial, double x) {
+double QHO::p(double x_trial, double x) {
 
   // compute the ratio of rho(xTrial) / rho(x)
-  return exp(- 2 * alpha * (xTrial*xTrial - x*x));
+  return exp(-2.0 * alpha_ * (x_trial*x_trial - x*x));
 }
 
-double QHO::eLocal(double xi) {
+double QHO::ELocal(double xi) {
 
   // compute the local energy
-  return alpha + xi * xi * (0.5 - 2 * alpha * alpha);
+  return alpha_ + xi * xi * (0.5 - 2 * alpha_ * alpha_);
 }
 
 void QHO::MetropolisStep() {
 
   // choose a walker at random
-  int n = int(dis(gen) * N);
+  int walker_n = int(uniform_(rng_) * N_);
 
   // make a trial move
-  double xTrial = x[n] + delta * gausdev(gen);
+  double x_trial = x_[walker_n] + delta_ * gaussian_(rng_);
 
   // Metropolis test
-  if (p(xTrial, x[n]) > dis(gen) ) {
-    x[n] = xTrial;
-    ++nAccept;
+  if (p(x_trial, x_[walker_n]) > uniform_(rng_) ) {
+    x_[walker_n] = x_trial;
+    ++n_accept_;
   }
 
   // accumulate energy and wave function
-  double e = eLocal(x[n]);
-  eSum += e;
-  eSqdSum += e * e;
-  int i = int((x[n] - xMin) / dx);
-  if (i >= 0 && i < nPsiSqd)
-    psiSqd[i] += 1;
+  double e = ELocal(x_[walker_n]);
+  E_sum_ += e;
+  E_squared_sum_ += e * e;
+  int i = int((x_[walker_n] - x_min_) / dx_);
+  if (i >= 0 && i < n_psi_squared_)
+    psi_squared_[i] += 1;
 }
 
 void QHO::oneMonteCarloStep() {
 
   // perform N Metropolis steps
-  for (int i = 0; i < N; i++) {
+  for (int i = 0; i < N_; i++) {
     MetropolisStep();
   }
 }
@@ -69,48 +72,48 @@ void QHO::oneMonteCarloStep() {
 void QHO::adjustStep() {
   // perform 20% of MCSteps as thermalization steps
   // and adjust step size so acceptance ratio ~50%
-  int thermSteps = int(0.2 * MCSteps);
-  int adjustInterval = int(0.1 * thermSteps) + 1;
+  int therm_steps = int(0.2 * MC_steps_);
+  int adjust_interval = int(0.1 * therm_steps) + 1;
 
-  std::cout << " Performing " << thermSteps << " thermalization steps ..."
+  std::cout << " Performing " << therm_steps << " thermalization steps ..."
 	    << std::flush;
-  for (int i = 0; i < thermSteps; i++) {
+  for (int i = 0; i < therm_steps; i++) {
     oneMonteCarloStep();
-    if ((i+1) % adjustInterval == 0) {
-      delta *= nAccept / (0.5 * N * adjustInterval) ;
-      nAccept = 0;
+    if ((i+1) % adjust_interval == 0) {
+      delta_ *= n_accept_ / (0.5 * N_ * adjust_interval) ;
+      n_accept_ = 0;
     }
   }
-  std::cout << "\n Adjusted Gaussian step size = " << delta << std::endl;    
+  std::cout << "\n Adjusted Gaussian step size = " << delta_ << std::endl;    
 }
 
 
 // production steps
 void QHO::doProductionSteps( ) {
   zeroAccumulators();
-  nAccept = 0;
-  std::cout << " Performing " << MCSteps << " production steps ..." << std::flush;
-  for (int i = 0; i < MCSteps; i++)
+  n_accept_ = 0;
+  std::cout << " Performing " << MC_steps_ << " production steps ..." << std::flush;
+  for (int i = 0; i < MC_steps_; i++)
     oneMonteCarloStep();
 }
 
 void QHO::printout() {
 
   // compute and print energy
-  double eAve = get_eAve(); 
-  double eVar = get_eVar(); 
-  double error = sqrt(eVar) / sqrt(double(N) * MCSteps);
-  std::cout << "\n <Energy> = " << eAve << " +/- " << error
-	    << "\n Variance = " << eVar << std::endl;
+  double E_avg = getEAvg(); 
+  double E_variance = getEVariance(); 
+  double error = sqrt(E_variance) / sqrt(double(N_) * MC_steps_);
+  std::cout << "\n <Energy> = " << E_avg << " +/- " << error
+	    << "\n Variance = " << E_variance << std::endl;
 
 }
 
 
 void QHO::normPsi() {
-  double psiNorm = 0;
-  for (int i = 0; i < nPsiSqd; i++)
-    psiNorm += psiSqd[i] * dx;
-  for (int i = 0; i < nPsiSqd; i++) {
-     psiSqd[i] /= psiNorm;
+  double psi_norm = 0;
+  for (int i = 0; i < n_psi_squared_; i++)
+    psi_norm += psi_squared_[i] * dx_;
+  for (int i = 0; i < n_psi_squared_; i++) {
+     psi_squared_[i] /= psi_norm;
   }
 }
--- QM/vmc.h
+++ QM/vmc.h
@@ -22,7 +22,7 @@ public :
   double p(double xTrial, double x);
 
   // local energy
-  double eLocal(double xi);
+  double ELocal(double xi);
 
   // Perform one Metropolis step
   void MetropolisStep(); 
@@ -41,37 +41,37 @@ public :
 
   void normPsi();
 
-  std::vector<double> const & get_psiSqd() const { return psiSqd; }
+  std::vector<double> const & getPsiSquared() const { return psi_squared_; }
 
-  double get_eAve() const { return eSum / double(N) / MCSteps;}
-  double get_eVar() const { double eAve = get_eAve(); return eSqdSum / double(N) / MCSteps - eAve * eAve;}
+  double getEAvg() const { return E_sum_ / double(N_) / MC_steps_;}
+  double getEVariance() const { double E_avg = getEAvg(); return E_squared_sum_ / double(N_) / MC_steps_ - E_avg * E_avg;}
 
 
-  double get_xMin() const { return xMin;}
-  double get_xMax() const { return xMax;}
-  double get_dx() const { return dx; }
+  double getXMin() const { return x_min_;}
+  double getXMax() const { return x_max_;}
+  double getDx() const { return dx_; }
   
 protected :
 
   // C++11 random number generator from Mersenne twister. 
-  std::random_device rd;
-  std::mt19937 gen;
-  std::uniform_real_distribution<> dis;
-  std::normal_distribution<> gausdev;
+  std::random_device rd_;
+  std::mt19937 rng_;
+  std::uniform_real_distribution<> uniform_;
+  std::normal_distribution<> gaussian_;
 
-  int N;                         // number of walkers
-  std::vector<double> x;         // walker positions
-  double delta;                  // step size
-  double eSum;                   // accumulator to find energy
-  double eSqdSum;                // accumulator to find fluctuations in E
-  double xMin = -10;             // minimum x for histogramming psi^2(x)
-  double xMax = +10;             // maximum x for histogramming psi^2(x)
-  double dx = 0.1;               // psi^2(x) histogram step size
-  std::vector<double> psiSqd;    // psi^2(x) histogram
-  int nPsiSqd;                   // size of array
-  double alpha;                  // trial function is exp(-alpha*x^2)
-  int nAccept;                   // accumulator for number of accepted steps
-  int MCSteps;                   // number of MC steps
+  int N_;                         // number of walkers
+  std::vector<double> x_;         // walker positions
+  double delta_;                  // step size
+  double E_sum_;                   // accumulator to find energy
+  double E_squared_sum_;                // accumulator to find fluctuations in E
+  double x_min_ = -10;             // minimum x for histogramming psi^2(x)
+  double x_max_ = +10;             // maximum x for histogramming psi^2(x)
+  double dx_ = 0.1;               // psi^2(x) histogram step size
+  std::vector<double> psi_squared_;    // psi^2(x) histogram
+  int n_psi_squared_;                   // size of array
+  double alpha_;                  // trial function is exp(-alpha*x^2)
+  int n_accept_;                   // accumulator for number of accepted steps
+  int MC_steps_;                   // number of MC steps
 };
   
 
