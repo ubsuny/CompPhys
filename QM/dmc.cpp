c72fa909ea753470d991ad8e15e59eb12815653e
DryRun <david.renhwa.yu@gmail.com>
2026-02-16 13:56:36 -0500

    Update QM diffusion notebook

--- QM/dmc.cpp
+++ QM/dmc.cpp
@@ -1,149 +1,154 @@
 // Diffusion Monte Carlo program for the 3-D harmonic oscillator
 #include "dmc.h"
 
-using namespace std;
-
-
-
-  
 DiffusionMC::DiffusionMC(int nt, double idt) :
-  rd(), gen(rd()), dis(0,1.), gausdev(),
-  N_T(nt), dt(idt)
+  rd_(), rng_(rd_()), uniform_(0,1.), gaussian_(),
+  N_T_(nt), dt_(idt)
 {
   std::cout << "Hello from DiffusionMC" << std::endl;
-  N = N_T;                   // set N to target number specified by user
-  ensureCapacity(N);    
-  for (int n = 0; n < N; n++) {      
+  N_ = N_T_;                   // set N to target number specified by user
+  ensureCapacity(N_);    
+  for (int n = 0; n < N_; n++) {      
     for (int d = 0; d < DIM; d++)
-      r[n][d] = dis(gen)- 0.5;
-    alive[n] = true;
+      r_walkers_[n][d] = uniform_(rng_)- 0.5;
+    alive_[n] = true;
   }
   zeroAccumulators();
-  E_T = 0;                   // initial guess for the ground state energy
+  E_T_ = 0;                   // initial guess for the ground state energy
 
 }
 
 
 
-double DiffusionMC::V( std::vector<double> const & r) {          // harmonic oscillator in DIM dimensions
-  double rSqd = 0;
-  for (int d = 0; d < DIM; d++)
-    rSqd += r[d] * r[d];
-  return 0.5 * rSqd;
+double DiffusionMC::V(std::vector<double> const & r) {          // harmonic oscillator in DIM dimensions
+  double r2 = 0;
+  for (int d = 0; d < DIM; d++) {
+    r2 += r[d] * r[d];
+  }
+  return 0.5 * r2;
 }
 
-void DiffusionMC::ensureCapacity(int index) {
+void DiffusionMC::ensureCapacity(unsigned int index) {
 
-  if (index < r.size())          // no need to expand array
+  if (index < r_walkers_.size()) {         // no need to expand array
     return;                // do nothing
-  else {
-    r.resize( 2 * index, std::vector<double> (DIM) );      
-    alive.resize( 2 * index, false );      
+  } else {
+    r_walkers_.resize(2 * index, std::vector<double>(DIM));      
+    alive_.resize(2 * index, false);      
     return;
   }
 }
   
 void DiffusionMC::zeroAccumulators() {
-  ESum = ESqdSum = 0;
-  psi.clear();
-  psi.resize(NPSI);
+  E_sum_ = 0;
+  E2_sum_ = 0;
+  psi_.clear();
+  psi_.resize(NPSI);
 }
 
 
 void DiffusionMC::oneMonteCarloStep(int n) {
 
   // Diffusive step
-  for (int d = 0; d < DIM; d++)
-    r[n][d] += gausdev(gen) * sqrt(dt);
+  for (int d = 0; d < DIM; d++) {
+    r_walkers_[n][d] += gaussian_(rng_) * sqrt(dt_);
+  }
 
   // Branching step
-  double q = exp(- dt * (V(r[n]) - E_T));
+  double q = exp(- dt_ * (V(r_walkers_[n]) - E_T_));
   int survivors = static_cast<int>(q);
-  if (q - survivors > dis(gen))
+  if (q - survivors > uniform_(rng_)) {
     ++survivors;
+  }
 
   // append survivors-1 copies of the walker to the end of the array
   for (int i = 0; i < survivors - 1; i++) {
-    ensureCapacity(N);
-    for (int d = 0; d < DIM; d++)
-      r[N][d] = r[n][d];
-    alive[N] = true;
-    ++N;
+    ensureCapacity(N_);
+    for (int d = 0; d < DIM; d++) {
+      r_walkers_[N_][d] = r_walkers_[n][d];
+    }
+    alive_[N_] = true;
+    ++N_;
   }
 
   // if survivors is zero, then kill the walker
-  if (survivors == 0)
-    alive[n] = false;
+  if (survivors == 0) {
+    alive_[n] = false;
+  }
 }
 
 void DiffusionMC::oneTimeStep() {
 
   // DMC step for each walker
-  int N_0 = N;
+  int N_0 = N_;
   for (int n = 0; n < N_0; n++)
     oneMonteCarloStep(n);
 
   // remove all dead walkers from the arrays
   int newN = 0;
-  for (int n = 0; n < N; n++)
-    if (alive[n]) {
+  for (int n = 0; n < N_; n++) {
+    if (alive_[n]) {
       if (n != newN) {
-	for (int d = 0; d < DIM; d++)
-	  r[newN][d] = r[n][d];
-	alive[newN] = true;
+	    for (int d = 0; d < DIM; d++) {
+	      r_walkers_[newN][d] = r_walkers_[n][d];
+        }
+	    alive_[newN] = true;
       }
       ++newN;
     }
-  N = newN;
+  }
+  N_ = newN;
 
   // adjust E_T
 
-  if ( N > 0 )
-    E_T += log(N_T / double(N)) / 10;
-
-  // measure energy, wave function
-  ESum += E_T;
-  ESqdSum += E_T * E_T;
-  for (int n = 0; n < N; n++) {
-    double rSqd = 0;
-    for (int d = 0; d < DIM; d++)
-      rSqd = r[n][d] * r[n][d];
-    int i = static_cast<int>(sqrt(rSqd) / rMax * NPSI);
-    if (i < NPSI)
-      psi[i] += 1;
+  if ( N_ > 0 ) {
+    E_T_ += log(N_T_ / double(N_)) / 10;
   }
-
+  E_T_history_.push_back(E_T_);
+    
+  // measure energy, wave function
+  E_sum_ += E_T_;
+  E2_sum_ += E_T_ * E_T_;
+  for (int n = 0; n < N_; n++) {
+    double r2 = 0;
+    for (int d = 0; d < DIM; d++) {
+      r2 = r_walkers_[n][d] * r_walkers_[n][d];
+    }
+    int i = static_cast<int>(sqrt(r2) / r_max_ * NPSI);
+    if (i < NPSI)
+      psi_[i] += 1;
+  }
 }
 
 void DiffusionMC::printout(std::ostream & out, int skip) {
-  if ( skip < 0 )
+  if (skip < 0) {
     skip = 1;
+  }
 
   double dr = getRMax() / getNPSI();
   for (int i = 0; i < getNPSI(); i+=skip) {
     double r = i * dr;
-    out << r << '\t' << psi[i]  << std::endl;
+    out << r << '\t' << psi_[i]  << std::endl;
   }    
        
 }
 
 
-void DiffusionMC::run( int timeSteps )
-{
+void DiffusionMC::run(int n_time_steps) {
   std::cout  << "Running" << std::endl;
   
   // do 20% of timeSteps as thermalization steps
-  int thermSteps = static_cast<int>(0.2 * timeSteps);
-  for (int i = 0; i < thermSteps; i++)
+  int n_therm_steps = static_cast<int>(0.2 * n_time_steps);
+  for (int i = 0; i < n_therm_steps; i++) {
     this->oneTimeStep();
+  }
 
   std::cout << "Initialization after thermalizing" << std::endl;
   this->printout(std::cout,10);
 
-    
   // production steps
   this->zeroAccumulators();
-  for (int i = 0; i < timeSteps; i++) {
+  for (int i = 0; i < n_time_steps; i++) {
     this->oneTimeStep();
 
     if ( i % 100 == 0 && i > 0 ) {
@@ -154,26 +159,27 @@ void DiffusionMC::run( int timeSteps )
   std::cout << "Final form" << std::endl;
   this->printout(std::cout,10);
 
-  std::cout << "ESum = " << this->getESum() << std::endl;
-  std::cout << "ESqdSum = " << this->getESqdSum() << std::endl;
+  std::cout << "E_sum_ = " << this->getESum() << std::endl;
+  std::cout << "E2_sum_ = " << this->getE2Sum() << std::endl;
     
   // compute averages
-  double EAve = this->getESum() / timeSteps;
-  double EVar = this->getESqdSum() / timeSteps - EAve * EAve;
-  cout << " <E> = " << EAve << " +/- " << sqrt(EVar / timeSteps) << endl;
-  cout << " <E^2> - <E>^2 = " << EVar << endl;
-  double psiNorm = 0, psiExactNorm = 0;
+  double E_avg = this->getESum() / n_time_steps;
+  double E_var = this->getE2Sum() / n_time_steps - E_avg * E_avg;
+  std::cout << " <E> = " << E_avg << " +/- " << sqrt(E_var / n_time_steps) << std::endl;
+  std::cout << " <E^2> - <E>^2 = " << E_var << std::endl;
+  double psi_norm = 0;
+  double psi_exact_norm = 0;
   double dr = this->getRMax() / this->getNPSI();
   for (int i = 0; i < this->getNPSI(); i++) {
     double r = i * dr;
-    psiNorm += pow(r, this->getDIM()-1) * psi[i] * psi[i];
-    psiExactNorm += pow(r, this->getDIM()-1) * exp(- r * r);
+    psi_norm += pow(r, this->getDIM()-1) * psi_[i] * psi_[i];
+    psi_exact_norm += pow(r, this->getDIM()-1) * exp(- r * r);
   }
-  psiNorm = sqrt(psiNorm);
-  psiExactNorm = sqrt(psiExactNorm);
+  psi_norm = sqrt(psi_norm);
+  psi_exact_norm = sqrt(psi_exact_norm);
 
   for ( int i = 0; i < this->getNPSI(); ++i ){
-    psi[i] /= psiNorm; 
+    psi_[i] /= psi_norm; 
   }
   
 }
--- QM/dmc.h
+++ QM/dmc.h
@@ -17,17 +17,18 @@ public:
 
   // harmonic oscillator in DIM dimensions
   double V( std::vector<double> const & r);
-  void ensureCapacity(int index);
+  void ensureCapacity(unsigned int index);
   void zeroAccumulators() ;
   void oneMonteCarloStep(int n);
   void oneTimeStep();
   
   inline int getDIM() const { return DIM;}
   inline int getNPSI() const { return NPSI;}
-  inline std::vector<double> const & getPsi() const { return psi;}
-  inline double getESum() const { return ESum;}
-  inline double getESqdSum() const { return ESqdSum;}
-  inline double getRMax() const { return rMax; }
+  inline std::vector<double> const & getPsi() const { return psi_;}
+  inline double getESum() const { return E_sum_;}
+  inline double getE2Sum() const { return E2_sum_;}
+  inline double getRMax() const { return r_max_; }
+  inline std::vector<double> const & getETHistory() const { return E_T_history_; }
 
 
   void printout(std::ostream & out, int skip = -1 );
@@ -37,28 +38,30 @@ public:
   
 protected:
   // C++11 random number generator from Mersenne twister. 
-  std::random_device rd;
-  std::mt19937 gen;
-  std::uniform_real_distribution<> dis;
-  std::normal_distribution<> gausdev;
+  std::random_device rd_;
+  std::mt19937 rng_;
+  std::uniform_real_distribution<> uniform_;
+  std::normal_distribution<> gaussian_;
   
   static const int DIM = 3;      // dimensionality of space
   static const int NPSI = 100;   // number of bins for wave function
 
   // random walkers
-  int N;                         // current number of walkers
-  int N_T;                       // desired target number of walkers
-  std::vector< std::vector<double> > r;   // x,y,z positions of walkers
-  std::vector<bool> alive;                // is this walker alive?
+  int N_;                         // current number of walkers
+  int N_T_;                       // desired target number of walkers
+  std::vector< std::vector<double> > r_walkers_;   // x,y,z positions of walkers
+  std::vector<bool> alive_;                // is this walker alive?
 
-  double dt;                              // Delta_t set by user
-  double E_T;                             // target energy
+  double dt_;                              // Delta_t set by user
+  double E_T_;                             // target energy
 
   // observables
-  double ESum;                            // accumulator for energy
-  double ESqdSum;                         // accumulator for variance
-  double rMax = 4;                        // max value of r to measure psi
-  std::vector<double> psi;                // wave function histogram
+  double E_sum_;                            // accumulator for energy
+  double E2_sum_;                         // accumulator for variance
+  double r_max_ = 4;                        // max value of r to measure psi
+  std::vector<double> psi_;                // wave function histogram
+  std::vector<double> E_T_history_; // store all E_T values
+  std::vector<std::vector<double>> psi_history_;
 
 };
 
