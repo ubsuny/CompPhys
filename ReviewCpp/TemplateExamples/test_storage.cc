6dfd29d9929c244b6d7dda4fe3678a374fe41508
DryRun <david.renhwa.yu@gmail.com>
2026-09-09 13:46:41 -0400

    Update C++ templating examples

--- ReviewCpp/TemplateExamples/test_storage.cc
+++ ReviewCpp/TemplateExamples/test_storage.cc
@@ -2,19 +2,13 @@
 #include "Storage.h"
 
 int main(void) {
-
-
-
-  Storage<int> intstorage( 5 );
+  Storage<int> intstorage(5);
   Storage<double> doublestorage( 4.6);
-
-
 
   std::cout << "Int storage: " << intstorage.get_val() << std::endl;
   std::cout << "double storage: " << doublestorage.get_val() << std::endl;
 
-
-  double x,y;
+  double x, y;
   std::cout << "Enter x and y:" << std::endl;
   std::cin >> x >> y;
   Point p(x,y);
