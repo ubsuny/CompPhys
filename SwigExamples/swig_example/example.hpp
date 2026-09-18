baa219eae3acdfc94c9bf5a10640d41f11d7f6a3
DryRun <david.renhwa.yu@gmail.com>
2026-09-18 12:36:54 -0400

    Python binding notebooks

--- SwigExamples/swig_example/example.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef EXAMPLE_HPP
-#define EXAMPLE_HPP
-
-#include <vector>
-
-// Declare your function normally
-int sum_int(std::vector<int> const & vec);
-
-#endif // EXAMPLE_HPP
--- SwigExamples/swig_example/test.py
+++
@@ -1,7 +0,0 @@
-from example import *
-import numpy as np
-
-a = vector_int([1,2,3])
-x = sum_int(a)
-print x
-
